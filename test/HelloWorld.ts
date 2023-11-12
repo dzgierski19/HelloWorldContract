ca71f050ff157ef1c2442579fbd69444a1e74489
dzgierski19 <dzgierskigit@gmail.com>
2023-11-12 12:24:38 +0100

    commit

---
+++ test/HelloWorld.ts
@@ -0,0 +1,84 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+// https://github.com/dethcrypto/TypeChain
+import { HelloWorld } from "../typechain-types";
+// https://hardhat.org/hardhat-network-helpers/docs/overview
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+
+// https://mochajs.org/#getting-started
+describe("HelloWorld", function () {
+  // https://hardhat.org/hardhat-network-helpers/docs/reference#fixtures
+  async function deployContract() {
+    // https://hardhat.org/plugins/nomiclabs-hardhat-ethers.html#helpers
+    const accounts = await ethers.getSigners();
+
+    // https://hardhat.org/plugins/nomiclabs-hardhat-ethers.html#helpers
+    const helloWorldFactory = await ethers.getContractFactory("HelloWorld");
+    // https://docs.ethers.org/v6/api/contract/#ContractFactory
+    const helloWorldContract = await helloWorldFactory.deploy();
+    // https://docs.ethers.org/v6/api/contract/#BaseContract-waitForDeployment
+    await helloWorldContract.waitForDeployment();
+
+    return { helloWorldContract, accounts };
+  }
+
+  it("Should give a Hello World", async function () {
+    // https://hardhat.org/hardhat-network-helpers/docs/reference#fixtures
+    const { helloWorldContract } = await loadFixture(deployContract);
+
+    // https://docs.ethers.org/v6/api/contract/#BaseContractMethod
+    const helloWorldText = await helloWorldContract.helloWorld();
+    // https://www.chaijs.com/api/bdd/#method_equal
+    expect(helloWorldText).to.equal("Hello World");
+  });
+
+  it("Should set owner to deployer account", async function () {
+    // https://hardhat.org/hardhat-network-helpers/docs/reference#fixtures
+    const { helloWorldContract, accounts } = await loadFixture(deployContract);
+
+    // https://docs.ethers.org/v6/api/contract/#BaseContractMethod
+    const contractOwner = await helloWorldContract.owner();
+    // https://www.chaijs.com/api/bdd/#method_equal
+    expect(contractOwner).to.equal(accounts[0].address);
+  });
+
+  it("Should not allow anyone other than owner to call transferOwnership", async function () {
+    // https://hardhat.org/hardhat-network-helpers/docs/reference#fixtures
+    const { helloWorldContract, accounts } = await loadFixture(deployContract);
+
+    // https://docs.ethers.org/v6/api/contract/#BaseContract-connect
+    // https://docs.ethers.org/v6/api/contract/#BaseContractMethod
+    // https://hardhat.org/hardhat-chai-matchers/docs/overview#reverts
+    await expect(
+      helloWorldContract
+        .connect(accounts[1])
+        .transferOwnership(accounts[1].address)
+    ).to.be.revertedWith("Caller is not the owner");
+  });
+
+  it("Should execute transferOwnership correctly", async function () {
+    const { helloWorldContract, accounts } = await loadFixture(deployContract);
+    const tx = await helloWorldContract.transferOwnership(ethers.ZeroAddress);
+    //now i wait for transaction to be included in the blockchain
+    await tx.wait();
+    const contractOwner = await helloWorldContract.owner();
+    // https://www.chaijs.com/api/bdd/#method_equal
+    expect(contractOwner).to.equal(ethers.ZeroAddress);
+  });
+
+  it("Should not allow anyone other than owner to change text", async function () {
+    const { helloWorldContract, accounts } = await loadFixture(deployContract);
+    await expect(
+      helloWorldContract.connect(accounts[1]).setText("I don't have permission")
+    ).to.be.revertedWith("Caller is not the owner");
+  });
+
+  it("Should change text correctly", async function () {
+    const { helloWorldContract } = await loadFixture(deployContract);
+    const newText = "Ethereum is the best";
+    const tx = await helloWorldContract.setText(newText);
+    await tx.wait();
+    const helloWorldText = await helloWorldContract.helloWorld();
+    expect(helloWorldText).to.equal("Ethereum is the best");
+  });
+});
